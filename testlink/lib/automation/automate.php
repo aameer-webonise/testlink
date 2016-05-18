24da5ef3886f7932aabc8446775c3e8368c5781b
aameer-webonise <aameer.ausekar@weboniselab.com>
2016-05-18 12:27:00 +0530

    add some improve code to check or uncheck child testcases

--- testlink/lib/automation/automate.php
+++ testlink/lib/automation/automate.php
@@ -1,7 +1,4 @@
 <?php
-//require_once('common.php');
-//$conn=connectToDB();
-
 require_once("../../config.inc.php");
 require_once("../functions/common.php");
 require_once("../functions/xml.inc.php");
@@ -19,40 +16,33 @@ $project_prefix=$myrow['prefix'];
 
 $sql='select id, name from nodes_hierarchy where node_type_id=2 and parent_id='.$tproject_id;
 $result = $db->exec_query($sql);
-$tree='<div><ul>';
+$tree='<div><ul style="list-style-type:none">';
 while($myrow = $db->fetch_array($result))
 {
 	$ID = $myrow['id'];
 	$name= $myrow['name'];
-	$query='select id, name from nodes_hierarchy where parent_id='.$ID.' and id in (select parent_id from nodes_hierarchy where id in (select id from tcversions where execution_type=2))';
-	//$query='select id, name from nodes_hierarchy where node_type_id=3 and parent_id='.$ID;
-	$data=$db->exec_query($query);
+	$sql='select id, name from nodes_hierarchy where parent_id='.$ID.' and id in (select parent_id from nodes_hierarchy where id in (select id from tcversions where execution_type=2))';
+	$data=$db->exec_query($sql);
 	if($data->_numOfRows>0)
 	{
 		$tree.='<label id="suite'.$ID.'">-</label><input type="checkbox" class="suiteCheckbox" id="ch-'.$ID.'"><span id="'.$ID.'" class="testSuiteName" style="cursor: pointer;">'.$name.'</span>';
-		$tree.='<ul style="list-style-type:none;" id="'.'tc'.$ID.'">';
+		$tree.='<li><ul style="list-style-type:none;" id="'.'tc'.$ID.'">';
 		while($row=$db->fetch_array($data))
 		{
 			$tree.='<li><input type="checkbox" class="checkbox-ch-'.$ID.'" name="testcase" value="'.$row['name'].'" id="'.$row['id'].'">'.$row['name'];
-			$query='select value from cfield_design_values inner join nodes_hierarchy on node_id=id where node_type_id=4 and parent_id='.$row['id'];
-			$fileNames=$db->exec_query($query);
+			$sql='select value from cfield_design_values inner join nodes_hierarchy on node_id=id where node_type_id=4 and parent_id='.$row['id'];
+			$fileNames=$db->exec_query($sql);
 			$fileRow=$db->fetch_array($fileNames);
 			$tree.='<input type="text" value="'.$fileRow['value'].'"></li>';
 		}
-		$tree.='</ul><br/>';
+		$tree.='</ul></li>';
 	}
 }
 $tree.='</ul></div>';
+$body='<body><form action="triggerAutomation.php" method="post"> <input type="text" name="projectName" value="'.$projectName.'" disabled>';
+$body.='<input type="hidden" value="'.$project_prefix.'" name="project_prefix"><input type="submit" value="Start Project Automation">'.$tree.'</form></body></html>';
 
-//$sql='select prefix from testprojects where id='.$tproject_id;
-//$db->exec_query($sql);
-
-//$keywords='#'.$projectName.'#Trigger#Automation';
-
-$body='<body><form action="triggerAutomation.php" method="post"> <input type="text" name="projectName" value="'.$projectName.'">';
-$body.='<input type="hidden" value="'.$project_prefix.'" name="project_prefix"><input type="submit" value="Start Project Automation"><br/>'.$tree.'</form></body></html>';
-
-$script='<html><head><script src="/testlink/third_party/jquery/jquery-2.0.3.min.js"></script>
+$script='<!DOCTYPE html><html><head><script src="/testlink/third_party/jquery/jquery-2.0.3.min.js"></script>
 <script>
 $(document).ready(function(){
     $(".testSuiteName").click(function(){
@@ -63,14 +53,7 @@ $(document).ready(function(){
 	
 	$(".suiteCheckbox").change(function(){
 		var id=$(this).attr("id");
-		
-		if ($(this).is(":checked")) 
-		{
-  			$(".checkbox-"+id).prop("checked", true);
-  		}
-		else{
-			$(".checkbox-"+id).prop("checked", false);
-		}
+		$(".checkbox-"+id).prop("checked", ($(this).is(":checked"))?true:false);
 	});
 });
 </script></head>';
